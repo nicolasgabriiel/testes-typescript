4ee20269c5a17d75f5daf700008500aa194feb8f
Nicolas <nicolasgabriel.developer@gmail.com>
2023-01-18 21:25:18 -0300

    configurando mais ainda o ts

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,12 +1,8 @@
 // import { NegociacaoController } from "./controllers/negociacao-controller.js";
 // console.log('aa')
 // const controller = new NegociacaoController
-
 // const form = document.querySelector('.form')
-
 // form.addEventListener('submit', event => {
 //     event.preventDefault();
 //     controller.adiciona();
 // })
-
-alert('oi')
